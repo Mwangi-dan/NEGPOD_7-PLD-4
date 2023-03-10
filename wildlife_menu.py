709169af155c0c5cebc1c23234d7fcf0b9677b09
Dan <d.ndungu@alustudent.com>
2023-03-10 16:40:32 +0200

    Terrestrial Trivia questions

--- wildlife_menu.py
+++ wildlife_menu.py
@@ -41,5 +41,63 @@ if second_option=="1":
     webbrowser.open_new("https://www.wildlifetrusts.org/learning")
 
 else:
-    webbrowser.open_new("https://lp.panda.org/wildlife-day-quiz")
+    # Terrestrial questions
+    score = 0
+
+    if option == '1':
+        print("TRIVIA QUESTIONS")
+
+        q1 = input("What is the colour of a giraffe's tongue? ") 
+        if q1 == 'purple':
+            print("Congratulations!\n")
+            score += 1
+        else:
+            print("Incorrect choice\n")
+
+        q2 = input("Which animal is the only animal that can’t jump? ")
+        if q2 == '':
+            print("Congratulations!\n")
+            score += 1
+        else:
+            print("Incorrect choice\n")
+
+        # ask team about this one
+        q3 = input("A cat’s urine doesn’t glow under black light? True or False: ")
+        if q2 == '':
+            print("Congratulations!\n")
+            score += 1
+        else:
+            print("Incorrect choice\n")
+
+        q4 = input("Which mammal has a cube-shaped poop? ")
+        if q4 == '':
+            print("Congratulations!\n")
+            score += 1
+        else:
+            print("Incorrect choice\n")
+
+        q5 = input("What mammal can catch leprosy? ")
+        if q5 == '':
+            print("Congratulations!\n")
+            score += 1
+        else:
+            print("Incorrect choice\n")
+
+        q6 = input("Dogs are not colour-blind but they can’t see one colour, What’s that colour? ")
+        if q6 == '':
+            print("Congratulations!\n")
+            score += 1
+        else:
+            print("Incorrect choice\n")
+
+        q7 = input("Koalas sleep partially the whole day. How many hours do you think they sleep up to? ")
+        if q7 == '':
+            print("Congratulations!\n")
+            score += 1
+        else:
+            print("Incorrect choice\n")
+
+        print("Well done on completing the trivia.\nYour total score is:\b {:d}".format(score))
+
+
 
